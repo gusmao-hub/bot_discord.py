0aa925944a9a5064442e577492198b8fdbc5b670
Renan Gusmão <rgusmao.contato@gmail.com>
2021-05-18 00:29:49 -0300

    update

--- main.py
+++ main.py
@@ -1,73 +1,78 @@
-from discord.ext import commands, tasks
-from discord import Intents, colour
 from TOKEN import token_oculto
+from discord.embeds import Embed
+from discord.ext import commands, tasks
+from discord import Intents, Colour
 import aiohttp
 import discord
 import random
+import os
+import youtube_dl
 
 token = token_oculto
 intents = discord.Intents.default()
 intents.members = True
-client = commands.Bot(command_prefix="!", help_command=None, intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 async def on_ready(): 
     print(f'{client.user.name} [ON SERVER]')
     print(f'Server ID - [{client.user.id}]')
 
-    #função async - on_ready (mostra no console se o bot está ONLINE)
-    #parametros usados para mostrar o nome do bot
-    #parametros usados para mostrar o ID server
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game('!comandos'))
 
-@client.event #trigger comunicação Discord X Bot
+@client.event
 async def on_member_join(member): 
-    await client.get_channel(839576746293723141).send(f'Seja bem vindo :) {member.mention}') 
+    embed = discord.Embed(
 
-    #função async - on_member_join (mostra uma mensagem de boas vindas para o usario recebendo o parametro do decorator)
-    #await é a resposta que o async solicita neste caso apontando para o canal dando get_channel + ID, usando o parametro de membro como mensagem a ser enviada + usuario
+        title='Seja bem vindo 😜' ,
+        description= member.mention,
+        color= discord.Colour.red()
+    )
+    await member.create_dm()
+    await member.dm_channel.send(embed=embed)
 
 @client.event
 async def on_member_remove(member):
-    await client.get_channel(839576746293723141).send(f'Foi tarde {member.mention} :)')
+    embed = discord.Embed(
 
-    #função async - on_member_remove (mostra uma mensagem de adeus para o usario recebendo o parametro do decorator)
-    #await é a resposta que o async solicita neste caso apontando para o canal dando get_channel + ID, usando o parametro de membro como mensagem a ser enviada + usuario
+        title='Foi embora 😔' ,
+        description= member.mention,
+        color= discord.Colour.red()
+    )
+    await member.create_dm()
+    await member.dm_channel.send(embed=embed)
 
-@client.command(pass_context= True) #pass_context = Não faço ideia do porque mas precisa estar como TRUE
-async def teste2(ctx): #comando criado que vai ser chamado com o prefixo >!<
+'''@client.command(pass_context= True) 
+async def teste2(ctx):
     embed = discord.Embed(  
 
-        title="Teste de Reações",  #titulo da box
-        description="Teste a Reação abaixo",  #descricao da box
-        color= discord.Colour.purple()  #cor da barra lateral
-
-    ) #embed ele permite a edicao da box de mensagem
-
-    msg = await ctx.send(embed=embed)  #Await retorno da mensagem com a edição de embed
-    await msg.add_reaction('1️⃣')  #Mensagem com a react.
-    await msg.add_reaction('2️⃣')  #Mensagem com a react.
+        title="Teste de Reações",  
+        description="Teste a Reação abaixo",  
+        color= discord.Colour.purple() 
+    ) 
+    msg = await ctx.send(embed=embed) 
+    await msg.add_reaction('1️⃣')  
+    await msg.add_reaction('2️⃣')  
 
 @client.event
 async def on_raw_reaction_add(payload):
-    msg_fixada = payload.message_id    #Atribuindo msg_fixada ao método payload.message_id
-    if msg_fixada == 841478314848813056:    #Atribuindo msg_fixada ao ID da mensagem fixada no canal
-        guild_id = payload.guild_id    #Atribuindo guild_id ao método payload.guild_id
-        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)    #Atribuindo a VAR guild aos métodos discord.utils.find com os seus parametros
-        member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)    #Atribuindo VAR member aos métodos discord.utils.find com os seus parametros
-        emoji = payload.emoji.name    #Atribuindo a VAR emoji aos métodos payload.emoji.name
+    msg_fixada = payload.message_id    
+    if msg_fixada == 841478314848813056:    
+        guild_id = payload.guild_id    
+        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+        member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)    
+        emoji = payload.emoji.name    
 
-        if emoji == '1️⃣':   #Utilizando a mensagem com embed aplicado esperando a reação [1]
-            role = discord.utils.get(guild.roles, name= 'cargo1') #VAR role aplicando os metodos do discord.utils.get(parametros de guild.role, name = nome do cargo)
-            await member.add_roles(role) #Await vai adicionar o membro na role de reação [1]
-        elif emoji == '2️⃣': #Utilizando a mensagem com embed aplicado esperando a reação [2]
-            role = discord.utils.get(guild.roles, name= 'cargo2') #VAR role aplicando os metodos do discord.utils.get(parametros de guild.role, name = nome do cargo)
-            await member.add_roles(role) #Await vai adicionar o membro na role de reação [2]
-
-#################################################################################################################################################################################
-#################################################################### - [ Lista de Comandos abaixo] - ############################################################################
-#################################################################################################################################################################################
+        if emoji == '1️⃣':
+            role = discord.utils.get(guild.roles, name= 'cargo1')
+            await member.add_roles(role) 
+        elif emoji == '2️⃣':
+            role = discord.utils.get(guild.roles, name= 'cargo2')
+            await member.add_roles(role)'''
 
 @client.command()
-async def help(ctx):
+async def comandos(ctx):
     embed = discord.Embed(
         title= 'Lista de Comandos:',
         description= '!teste 🚀\n !cafe ☕\n !ola 👋 \n !gatinho 😺 \n !d20 🎲 \n !moeda 🕹️ \n',
@@ -81,17 +86,12 @@ async def d20(ctx):
     await ctx.send(f'Você rolou um {dado}')
 
 @client.command()
-async def teste(ctx):
-    teste = random.randint(1, 9999)
-    await ctx.send(f'Estamos no teste {teste} 🚀')
-
-@client.command()
 async def cafe(ctx):
     await ctx.send('Pausa para o ☕')
 
 @client.command()
 async def bot(ctx):
-    await ctx.send(f'Posso ser um bot mas pelo menos não sou gado.')
+    await ctx.send(f'Oie eu sou o bot criado pelo Gusmão espero te ajudar 🧐')
 
 @client.command()
 async def moeda(ctx):
@@ -106,11 +106,11 @@ async def moeda(ctx):
 async def gatinho(ctx):
     async with ctx.channel.typing():
         async with aiohttp.ClientSession() as cs: #aiohttp.ClientSession() é -> cs
-            async with cs.get('http://aws.random.cat/meow') as request: #cs.get + URL é ->
+            async with cs.get('http://aws.random.cat/meow') as request: #cs.get + URL é -> request
                 data = await request.json()
 
                 embed = discord.Embed(title='🐱')
                 embed.set_image(url=data['file'])
                 await ctx.send(embed=embed)
-        
+
 client.run(token)
